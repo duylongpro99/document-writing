a8483ea39633af8882ba0ea9a9433599c9bfd17c
cris <ripad3011@gmail.com>
2025-03-05 20:31:18 +0700

    feat: templates gallery

---
+++ src/app/(home)/TemplatesGallery.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { DOCUMENT_TEMPLATES } from "@/constants/templates";
+import { cn } from "@/lib/utils";
+
+type Props = {};
+
+export const TemplatesGallery: React.FC<Props> = ({}) => {
+  const isCreating = false;
+
+  return (
+    <div className="bg-[#F1F3F4]">
+      <div className="max-w-screen-xl mx-auto px-16 py-6 flex flex-col gap-y-4">
+        <h3 className="font-medium">Start a new document</h3>
+        <Carousel>
+          <CarouselContent className="-ml-4">
+            {DOCUMENT_TEMPLATES.map((tpl) => {
+              return (
+                <CarouselItem
+                  key={tpl.id}
+                  className="basis-1/2 sm:basis-1/3 md:basis-1/4 lg:basis-1/5 xl:basis-1/6 2xl:basis-[14.285714%] pl-4"
+                >
+                  <div
+                    className={cn(
+                      "aspect-[3/4] flex flex-col gap-y-2.5",
+                      isCreating && "pointer-events-none opacity-50"
+                    )}
+                  >
+                    <button
+                      disabled={isCreating}
+                      onClick={() => {}}
+                      style={{
+                        backgroundImage: `url(${tpl.imageUrl})`,
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                        backgroundRepeat: "no-repeat",
+                      }}
+                      className="size-full hover:border-blue-500 rounded-sm border hover:bg-blue-50 transition flex flex-col items-center justify-center gap-y-4 bg-white"
+                    ></button>
+                    <p className="text-sm font-medium truncate">{tpl.label}</p>
+                  </div>
+                </CarouselItem>
+              );
+            })}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
+    </div>
+  );
+};
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,5 @@
 import { Navbar } from "./Navbar";
+import { TemplatesGallery } from "./TemplatesGallery";
 
 const Page: React.FC = () => {
   return (
@@ -6,7 +7,9 @@ const Page: React.FC = () => {
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
         <Navbar />
       </div>
-      <div className="mt-16">text</div>
+      <div className="mt-16">
+        <TemplatesGallery />
+      </div>
     </div>
   );
 };
