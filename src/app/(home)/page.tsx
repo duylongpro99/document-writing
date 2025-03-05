6b4d0d7d5e19623cc8f320675aeb4b634e35db57
cris <ripad3011@gmail.com>
2025-03-05 20:58:04 +0700

    feat: define document schema

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import { useQuery } from "convex/react";
 import { Navbar } from "./Navbar";
 import { TemplatesGallery } from "./TemplatesGallery";
+import { api } from "../../../convex/_generated/api";
 
 const Page: React.FC = () => {
+  const documents = useQuery(api.document.list);
+
+  if (documents === undefined) return <span>Loading...</span>;
+
   return (
     <div className="flex flex-col min-h-screen ">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
@@ -9,6 +17,10 @@ const Page: React.FC = () => {
       </div>
       <div className="mt-16">
         <TemplatesGallery />
+
+        {documents?.map((document) => {
+          return <span>{document.title}</span>;
+        })}
       </div>
     </div>
   );
