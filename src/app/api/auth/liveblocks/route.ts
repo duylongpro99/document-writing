aa61e97b8578dfa622f1d44d48838ebc835dd80c
cris <ripad3011@gmail.com>
2025-03-12 06:56:25 +0700

    reset env

--- src/app/api/auth/liveblocks/route.ts
+++ src/app/api/auth/liveblocks/route.ts
@@ -3,7 +3,7 @@ import { Liveblocks } from "@liveblocks/node";
 import { ConvexHttpClient } from "convex/browser";
 import { api } from "../../../../../convex/_generated/api";
 
-const convex = new ConvexHttpClient("https://zany-dotterel-919.convex.cloud");
+const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 const liveblocks = new Liveblocks({
   secret: process.env.LIVE_BLOCK_SECRET_API_KEY!,
 });
