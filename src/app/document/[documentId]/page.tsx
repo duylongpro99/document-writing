1b0b2bb310dfb3e516e415547d14e2ecd53fcfca
cris <ripad3011@gmail.com>
2025-03-11 20:49:22 +0700

    feat: room document with multiple users, update document realtime

--- src/app/document/[documentId]/page.tsx
+++ src/app/document/[documentId]/page.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from "./(navbar)/navbar";
 import { Toolbar } from "./(toolbar)/toolbar";
 import { Editor } from "./editor";
+import { Room } from "./room";
 
 interface Props {
   params: Promise<{ documentId: string }>;
@@ -10,16 +11,18 @@ const Page: React.FC<Props> = async ({ params }) => {
   console.info(":Document:", documentId);
 
   return (
-    <div className="min-h-screen bg-[#fafbfd]">
-      <div className="flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 z-10 bg-[#fafbfd] print:hidden">
-        <Navbar />
-        <Toolbar />
-      </div>
+    <Room>
+      <div className="min-h-screen bg-[#fafbfd]">
+        <div className="flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 z-10 bg-[#fafbfd] print:hidden">
+          <Navbar />
+          <Toolbar />
+        </div>
 
-      <div className="pt-[114px] print:pt-0">
-        <Editor />
+        <div className="pt-[114px] print:pt-0">
+          <Editor />
+        </div>
       </div>
-    </div>
+    </Room>
   );
 };
 
