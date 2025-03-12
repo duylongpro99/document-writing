c1df0734d82b618df92cbba7868e299042e7f75a
cris <ripad3011@gmail.com>
2025-03-12 07:43:28 +0700

    comment route to check env

--- src/app/document/[documentId]/room.tsx
+++ src/app/document/[documentId]/room.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-import { FullscreenLoader } from "@/components/fullscreen-loader";
-import { useToast } from "@/hooks/use-toast";
-import {
-  ClientSideSuspense,
-  LiveblocksProvider,
-  RoomProvider,
-} from "@liveblocks/react/suspense";
-import { useParams } from "next/navigation";
-import { ReactNode, useEffect, useMemo, useState } from "react";
-import { getUsers } from "./action";
+import { ReactNode } from "react";
+
+// import { FullscreenLoader } from "@/components/fullscreen-loader";
+// import { useToast } from "@/hooks/use-toast";
+// import {
+//   ClientSideSuspense,
+//   LiveblocksProvider,
+//   RoomProvider,
+// } from "@liveblocks/react/suspense";
+// import { useParams } from "next/navigation";
+// import { ReactNode, useEffect, useMemo, useState } from "react";
+// import { getUsers } from "./action";
 
 type User = {
   id: string;
@@ -18,56 +20,56 @@ type User = {
 };
 
 export function Room({ children }: { children: ReactNode }) {
-  const params = useParams<{ documentId: string }>();
-  const { toast } = useToast();
+  // const params = useParams<{ documentId: string }>();
+  // const { toast } = useToast();
 
-  const [users, setUsers] = useState<User[]>([]);
+  // const [users, setUsers] = useState<User[]>([]);
 
-  const fetchUsers = useMemo(
-    () => async () => {
-      try {
-        const users = await getUsers();
-        setUsers(users);
-      } catch {
-        toast({
-          title: "Error",
-          description: "Failed to fetch users",
-          variant: "destructive",
-        });
-      }
-    },
-    [toast]
-  );
+  // const fetchUsers = useMemo(
+  //   () => async () => {
+  //     try {
+  //       const users = await getUsers();
+  //       setUsers(users);
+  //     } catch {
+  //       toast({
+  //         title: "Error",
+  //         description: "Failed to fetch users",
+  //         variant: "destructive",
+  //       });
+  //     }
+  //   },
+  //   [toast]
+  // );
 
-  useEffect(() => {
-    fetchUsers();
-  }, [fetchUsers]);
+  // useEffect(() => {
+  //   fetchUsers();
+  // }, [fetchUsers]);
 
   return (
-    <LiveblocksProvider
-      authEndpoint={"/api/auth/liveblocks"}
-      throttle={16}
-      resolveUsers={({ userIds }) =>
-        userIds.map((id) => users.find((u) => u.id === id))
-      }
-      resolveMentionSuggestions={({ text }) => {
-        let filterUsers = users;
-        if (text) {
-          filterUsers = users.filter((u) =>
-            u.name.toLowerCase().includes(text.toLowerCase())
-          );
-        }
-        return filterUsers.map((u) => u.id);
-      }}
-      resolveRoomsInfo={() => []}
-    >
-      <RoomProvider id={params.documentId}>
-        <ClientSideSuspense
-          fallback={<FullscreenLoader label="Document loading..." />}
-        >
-          {children}
-        </ClientSideSuspense>
-      </RoomProvider>
-    </LiveblocksProvider>
+    // <LiveblocksProvider
+    //   authEndpoint={"/api/auth/liveblocks"}
+    //   throttle={16}
+    //   resolveUsers={({ userIds }) =>
+    //     userIds.map((id) => users.find((u) => u.id === id))
+    //   }
+    //   resolveMentionSuggestions={({ text }) => {
+    //     let filterUsers = users;
+    //     if (text) {
+    //       filterUsers = users.filter((u) =>
+    //         u.name.toLowerCase().includes(text.toLowerCase())
+    //       );
+    //     }
+    //     return filterUsers.map((u) => u.id);
+    //   }}
+    //   resolveRoomsInfo={() => []}
+    // >
+    //   <RoomProvider id={params.documentId}>
+    //     <ClientSideSuspense
+    //       fallback={<FullscreenLoader label="Document loading..." />}
+    //     >
+    <>{children}</>
+    //     </ClientSideSuspense>
+    //   </RoomProvider>
+    // </LiveblocksProvider>
   );
 }
