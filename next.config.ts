2eddce0fcf1985d63937cb51a6eb546cda660555
cris <ripad3011@gmail.com>
2025-03-15 11:30:17 +0700

    chore

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: "standalone",
+  // env: {
+  //   NEXT_PUBLIC_CONVEX_URL: process.env.NEXT_PUBLIC_CONVEX_URL,
+  //   CONVEX_DEPLOYMENT: process.env.CONVEX_DEPLOYMENT,
+  //   CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+  //   NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+  //     process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  //   LIVE_BLOCK_SECRET_API_KEY: process.env.LIVE_BLOCK_SECRET_API_KEY,
+  // },
 };
 
 export default nextConfig;
